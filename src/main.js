bbbf63806c5e6034ed88520da53057b1280f5be0
Bely <fbelen.andreozzi@gmail.com>
2022-08-09 18:11:05 -0300

    cambios finales v1

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const finalizarCompra = document.getElementById("boton-finalizar-compra")
 
 
 
-let cart =  JSON.parse(localStorage.getItem("Carrito")) || [];
-renderCart(); 
+let cart = JSON.parse(localStorage.getItem("Carrito")) || [];
+renderCart();
 
 //   ======= Productos de la tienda ========
 (async () => {
@@ -22,84 +22,85 @@ renderCart();
             title: '¡ERROR!',
             text: 'Algo salió mal. Inténtalo de nuevo más tarde',
             icon: 'error',
-            confirm: 'Ok', 
+            confirm: 'Ok',
             timer: 5000
         })
     }
-}) ()
+})()
 
 
 
 // Mostrar los productos en el DOM. 
 
 function renderProducts(data) {
-    
-    const shop = document.getElementById("productos_tienda");
-    
 
-    data.forEach((product) => {
+    const shop = document.getElementById("productos_tienda");
+
+
+    data.forEach((item) => {
         let producto =
             `
             <div class="card border-warning" style="width: 18rem;">
-                <img src="${product.img}" class="card-img-top p_foto" alt="imagen del producto">
+                <img src="${item.img}" class="card-img-top p_foto" alt="imagen del producto">
                 <div class="card-body">
-                    <h5 class="card-title">${product.nombre}</h5>
-                    <p class="card-text">$${product.precio}.</p>
-                    <button class="p_boton" id=${product.id}>Añadir al carrito</button>
+                    <h5 class="card-title">${item.nombre}</h5>
+                    <p class="card-text">$${item.precio}</p>
+                    <button class="p_boton" id=${item.id}>Añadir al carrito</button>
                 </div>
             </div>
         `
 
         shop.innerHTML += producto;
+        eventRemoveItem()
     });
-    globalEvents()
-}
+    eventAddToCart()
+};
 
-function globalEvents() {
-    let buttons = document.getElementsByClassName('p_boton'); 
-    console.log(buttons); 
-    for (const element of buttons){
-    element.addEventListener('click', ()=>{
-        console.log(element.id); 
-        addToCart(element.id)
-        
-    })}};
 
 //   ======= Funciones del carrito. =========
+
+// Evento de click para el botón de añadir al carrito
+function eventAddToCart() {
+    let buttons = document.getElementsByClassName('p_boton');
+    for (const element of buttons) {
+        element.addEventListener('click', () => {
+            addToCart(element.id)
+
+        })
+    }
+
+    for (const element of addItemAlertButton) {
+        element.addEventListener('click', (e) => {
+            Toastify({
+                text: "Agregaste un producto al carrito",
+                duration: 2000,
+                gravity: "bottom",
+                position: "left",
+                style: { background: "#f6d365" },
+                stopOnFocus: true,
+                close: true,
+            }).showToast();
+        });
+    }
+};
 
 //Agregar productos al carrito. 
 function addToCart(id) {
     let data = JSON.parse(localStorage.getItem("data"))
     if (data) {
-        
         if (cart.some((item) => item.id == id)) {
             quantityChangeInCart("plus", id)
         } else {
             const item = data.find((producto) => producto.id == id);
-    
+
             cart.push({
                 ...item,
                 cantidad: 1,
             });
         }
-    
+
         renderCart();
     }
-
-}
-
-for (const element of addItemAlertButton) {
-    element.addEventListener('click', (e)=> {
-        Toastify({
-            text: "Agregaste un producto al carrito",
-            duration: 2000,
-            gravity: "bottom",
-            position: "left",
-            style: {background: "#f6d365"},
-            stopOnFocus: true,
-            close: true,
-        }).showToast();
-    });
 }
 
 //Actualizar el carrito en el DOM.
@@ -115,37 +116,37 @@ function renderCart() {
 //Mostrar los items en el carrito
 function renderCartItem() {
     cartItem.innerHTML = "";
-    
+
     cart.forEach((item) => {
         cartItem.innerHTML += `
-        <li class="p_cart_info nav-link d-flex flex-wrap flex-row">
-            <div class="p-top-cart">        
-                <div class="col-12 text-dark text-center p-0">${item.nombre}</div>
-                <div onclick="removeItem(${item.id})"> <img src="../logos/eliminar.png" class="footer_logos_info"></div>
-            </div>
-        
-            <div class="col-4 p-0 p-img-cart"> <img class="img-fluid" src="${item.img}" alt="${item.nombre}"> </div>
-            <div class="col-8">
-                <div class="col-12 text-dark d-flex">
-                    <div class="btn minus m-0 p-0 col-4" onclick="quantityChangeInCart('minus', ${item.id})">-</div>
-                    <div class=" m-0 p-0 h5 col-4">${item.cantidad}</div>
-                    <div class="btn plus m-0 p-0 col-4" onclick="quantityChangeInCart('plus', ${item.id})">+</div>
+        <h5 class="m-0 text-center">${item.nombre}</h5>
+        <div class="m-0 card mb-3 border-warning" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${item.img}" alt="${item.nombre}" class="img-fluid rounded-start"> 
                 </div>
-
-                <div class="sidecart-price pl-0 col-12  text-right d-flex flex-wrap text-light">
-                    <div class="product-price text-dark">Valor unidad: $${item.precio}</div> 
+                <div class="col-md-8 d-flex flex-col align-items-center">
+                    <div class="card-body col-12 d-flex">   
+                        <div class="btn minus m-0 p-0 col-3 border-danger" onclick="quantityChangeInCart('minus', ${item.id})">-</div>
+                        <div class=" m-0 p-0 h5 col-3 text-center">${item.cantidad}</div>
+                        <div class="btn plus m-0 p-0 col-3 border-success" onclick="quantityChangeInCart('plus', ${item.id})">+</div>
+                        <div class="remove_item col-4 d-flex justify-content-center" id=${item.id}> <img src="../logos/eliminar.png" class="eliminar-item-logo"></div>
+                    </div>
+                    <p class="m-0"><small class="text-muted">Valor unidad: $${item.precio}</small></p>
                 </div>
             </div>
-        </li>
+        </div>
         `
     })
+
+    eventRemoveItem();
 }
 
 // Incremento o decremento de unidades en el carrito. 
 function quantityChangeInCart(action, id) {
-    
-    cart = cart.map((item) =>{
-        
+
+    cart = cart.map((item) => {
+
         let cantidad = item.cantidad
         if (item.id == id) {
             if (action === "minus" && cantidad > 1) {
@@ -153,11 +154,13 @@ function quantityChangeInCart(action, id) {
             } else if (action === "plus") {
                 cantidad++
             }
-            
+
         }
 
-        return {...item,
-        cantidad: cantidad};
+        return {
+            ...item,
+            cantidad: cantidad
+        };
     });
 
     renderCart()
@@ -167,8 +170,8 @@ function quantityChangeInCart(action, id) {
 function renderTotalPrice() {
     let totalPrice = 0;
     let totalItems = 0;
-    
-    cart.forEach((item) =>{
+
+    cart.forEach((item) => {
         totalPrice += item.precio * item.cantidad;
         totalItems += item.cantidad;
     });
@@ -181,21 +184,24 @@ function renderTotalPrice() {
     id="sidecart-total">$${totalPrice}</span></div>
     <div class="p-2">
 `
-} 
+}
 
-//Eliminación de un producto del carrito
+//Eliminación de un producto del carrito AYUDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+function eventRemoveItem() {
+    
+    const removeItemButton = document.getElementsByClassName('remove_item');
+    for (const element of removeItemButton) {
+        element.addEventListener('click', () => {
+            removeItem(element.id)
+        })
+    }
+}
+
 function removeItem(id) {
-    cart = cart.filter((item) => item.id !== id ) //Mira los items y se queda con los de la id distinta al eliminado
+    cart = cart.filter((item) => item.id !== id) //Mira los items y se queda con los de la id distinta al eliminado
 
     renderCart()
 }
-
-//Side cart (predeterminado)
-function toggleCart() {
-    document.querySelector('.sidecart').classList.toggle('open-cart');
-}
-
-toggleCart();
 
 //Reset carrito de compra
 function cartReset() {
@@ -210,25 +216,25 @@ reiniciarCarrito.addEventListener("click", cartReset);
 //Finalización de compra
 finalizarCompra.addEventListener("click", checkout);
 
-function checkout(){
+function checkout() {
     if (cart.length >= 1) {
-            swal({
-                title: 'Genial',
-                text: '¡Haz realizado la compra con exito!',
-                icon: 'success',
-                confirm: 'Ok', 
-                timer: 3000
-            });
-    
+        swal({
+            title: 'Genial',
+            text: '¡Haz realizado la compra con exito!',
+            icon: 'success',
+            confirm: 'Ok',
+            timer: 3000
+        });
+
         cartReset();
     } else {
         swal({
             title: '¡Cuidado!',
             text: 'No tienes ningún producto en el carrito',
             icon: 'error',
-            confirm: 'Ok', 
+            confirm: 'Ok',
             timer: 3000
         })
     }
-    
+
 }
